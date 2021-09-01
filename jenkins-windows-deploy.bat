b008d541738067333db60f7af3e593b59ed796e8
Hony-Ran <54339605+Hony-Ran@users.noreply.github.com>
2021-09-01 10:16:53 +0400

    Update jenkins-windows-deploy.bat
    改文字

--- jenkins-windows-deploy.bat
+++ jenkins-windows-deploy.bat
@@ -10,7 +10,7 @@ if exist %WORKSPACE%\target (
 ) else (
 	mkdir %WORKSPACE%\target								
 	) 	
-C:\Windows\System32\xcopy.exe  /Y /E C:\Works\workspace\PRE_All_source\server\kk1preview\40010000_多人李逵劈鱼\%DLLFILE% %WORKSPACE%\target 
+C:\Windows\System32\xcopy.exe  /Y /E C:\Works\workspace\PRE_All_source\server\kk1preview\40010000\%DLLFILE% %WORKSPACE%\target 
 C:\Windows\System32\xcopy.exe  /Y /E C:\Works\workspace\PRE_All_source\server\kk1preview\release\AFCComDllEx.dll %WORKSPACE%\target
 C:\Windows\System32\xcopy.exe  /Y /E C:\Works\workspace\PRE_All_source\server\kk1preview\release\GameManage.dll %WORKSPACE%\target
 if exist %path% (
